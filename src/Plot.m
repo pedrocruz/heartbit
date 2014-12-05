64fc248a3a6cc2a6126a2b183c0ac1a4c9b052f8
Pedro Cruz <pcruz@inoa.com.br>
2014-12-05 13:03:40 -0200

    Plotting the F{dECG}

--- src/Plot.m
+++ src/Plot.m
@@ -1,12 +1,13 @@
 load ('100m.mat')           % the signal will be loaded to "val" matrix
+totalPlots = 4;              % the total of graphics plotted
 val = (val - 1024)/200;     % you have to remove "base" and "gain"
 ECG = val(1,1:1000);        % select the lead (Lead I)
 Fs = 360;                   % sampling frequecy
 t = (0:length(ECG)-1)/Fs;   % creatin the time array
 
 figure                      % create new figure
-subplot(2,1,1)              % first subplot
-plot(t,ECG)                % plotting the signal
+subplot(totalPlots,1,1)     % first subplot
+plot(t,ECG)                 % plotting the signal
 title('ECG')
 dECG = zeros(size(ECG));    % starting an array to store ECG's derivative
 halfF = Fs/2;               % this will be used to calculate dECG
@@ -14,12 +15,17 @@ halfF = Fs/2;               % this will be used to calculate dECG
 disp(['time interval = ', num2str(halfF)]);
 disp(['size(ECG) = ', num2str(size(ECG))]);
 
-sizeECG = size(ECG);
-for i = 2:(sizeECG(2)-1)    % this loop calculates dECG
+for i = 2:(length(ECG)-1)    % this loop calculates dECG
     dECG(i) = (halfF) * (ECG(i+1) - ECG(i-1));
-    disp(['ECG(i-1) = ', num2str(ECG(i-1)), 'ECG(i+1)',num2str(ECG(i+1))]);
 end
-subplot(2,1,2)              % second subplot
-plot(t, dECG);
-title('dECG')
+subplot(totalPlots,1,2)     % second subplot
+plot(t, dECG);              % plotting dECG
+title('dECG')               % adding a title to the graphic
+
+FdECG = fft(dECG);          % getting the fourier transform of dECG
+f = (0:length(FdECG)-1)*Fs/length(ECG);
+display(['len(f) = ', num2str(length(f)), 'len(FdECG) = ', num2str(length(FdECG))]);
+subplot(totalPlots,1,3);    % third subplot
+plot(f, FdECG);              % plotting the fourier transform of dECG
+title('F{dECG}')
     
