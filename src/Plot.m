b542f632b4db25c87b478a14ca1dee6c67c7acaf
Pedro Cruz <pcruz@inoa.com.br>
2014-12-08 15:17:30 -0200

    More data files

--- src/Plot.m
+++ src/Plot.m
@@ -2,8 +2,30 @@
 % Pedro Cruz
 % Thiago Barroso Perrotta
 
+% Names of the files that will be used
+data = ['Data\100.mat', 'Data\101.mat', 'Data\102.mat' ...
+    'Data\103.mat', 'Data\104.mat', 'Data\105.mat' ...
+    'Data\106.mat', 'Data\107.mat', 'Data\108.mat' ...
+    'Data\109.mat', 'Data\111.mat', 'Data\112.mat' ...
+    'Data\113.mat', 'Data\114.mat', 'Data\115.mat' ...
+    'Data\116.mat', 'Data\117.mat', 'Data\118.mat' ...
+    'Data\119.mat', 'Data\121.mat', 'Data\122.mat' ...
+    'Data\123.mat', 'Data\124.mat', 'Data\200.mat' ...
+    'Data\201.mat', 'Data\202.mat', 'Data\203.mat' ...
+    'Data\205.mat', 'Data\207.mat', 'Data\208.mat'];
+
+diag = ['Supraventricular ectopy', 'Supraventricular ectopy', 'Ventricular ectopy'...
+   'Supraventricular ectopy', 'Ventricular ectopy', 'Ventricular ectopy'...
+   'Ventricular ectopy', 'Ventricular ectopy', 'Supraventricular ectopy'...
+   'Ventricular ectopy', 'Normal', 'Supraventricular ectopy' ...
+   'Supraventricular ectopy', 'Supraventricular ectopy', 'Normal' ...
+   'Ventricular ectopy', 'Normal', 'Supraventricular ectopy' ...
+   'Ventricular ectopy', 'Normal', 'Normal' ...
+   'Ventricular ectopy', 'Ventricular ectopy', 'Ventricular ectopy' ...
+   'Supraventricular ectopy', 'Supraventricular ectopy', 'Ventricular ectopy' ...
+   'Ventricular ectopy', 'Ventricular ectopy', 'Ventricular ectopy'];
 % Load data to the 'val' Matrix.
-load '100m.mat'
+load 'Data\102m.mat'
 
 % The total of graphics to be plotted.
 totalPlots = 5;
@@ -34,18 +56,27 @@ FdECG = fft(dECG);          % getting the fourier transform of dECG
 f = (0:length(FdECG)-1)*Fs/length(ECG);
 subplot(totalPlots,1,3);    % third subplot
 plot(f, real(FdECG));       % plotting the fourier transform of dECG
-title('F{dECG}')
+title('F\{dECG\}')
 
 HdECG = imag(hilbert(dECG));% taking the Hilbert transform of the signal
 subplot (totalPlots, 1, 4); % fourth subplot
 plot(t, HdECG);             % plotting the Hilbert transform
+title('H\{dECG\}')
 
 BdECG = abs(hilbert(dECG)); % CReating the envelope
 subplot (totalPlots, 1, 5); % fifth subplot
 plot(t, BdECG);             % plotting the envelope of Hilbert transform
+title('Envelope\{dECG\}')
 
 % http://www.ehow.com.br/detectar-pico-matlab-como_33112/
 menor = min(BdECG);
 maior = max(BdECG);
 media = (menor + maior) / 2.0;
 [peak_value, peak_location] = findpeaks(BdECG,'minpeakheight',media);
+
+
+
+
+fid = fopen('variances.txt','wt');  % Note the 'wt' for writing in text mode
+fprintf(fid,'%f\n',a);  % The format string is applied to each element of a
+fclose(fid);
